bbb23a163247538ba832330cd6e83f3a947c076d
AlexanderKuchun <80195385+AlexanderKuchun@users.noreply.github.com>
2021-11-04 22:19:34 +0300

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+import java.lang.String;
+
 import java.io.*;
 import java.sql.*;
 
@@ -16,13 +19,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SecondTest {
+
     private String targetUrl;
-    private String result;
-    private String expectedUSD;
-    private String expectedEUR;
+    private String dataFromTheSite;
+    private String usdFromFile;
+    private String eurFromFile;
     private String usdFromDatabase;
     private String eurFromDatabase;
-    static String ADDRESS = "D:/Projects/url.json";
+    static String pathToTheFile = "D:/Projects/Folder/file1.json";
+
     private static final int PAUSEINMILLISECONDS = 1000;
 
     @Before
@@ -30,13 +35,13 @@ public class SecondTest {
 
 
         try {
-            FileReader reader = new FileReader(ADDRESS);
+            FileReader reader = new FileReader(pathToTheFile);
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
             this.targetUrl = (String) jsonObject.get("url");
-            this.result = (String) jsonObject.get("file");
-            this.expectedUSD = (String) jsonObject.get("1 USD");
-            this.expectedEUR = (String) jsonObject.get("1 EUR");
+            this.dataFromTheSite = (String) jsonObject.get("file");
+            this.usdFromFile = (String) jsonObject.get("1 USD");
+            this.eurFromFile = (String) jsonObject.get("1 EUR");
 
         } catch (ParseException | IOException | NullPointerException ex) {
             ex.printStackTrace();
@@ -44,15 +49,15 @@ public class SecondTest {
         }
 
 
-        String username = "postgres";
-        String password = "Vasilchuk1";
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String USERNAME = "postgres";
+        String PASSWORD = "Vasilchuk1";
+        String URL = "jdbc:postgresql://localhost:5432/postgres";
 
         Connection connection = null;
         ResultSet resultSet = null;
         ResultSet resultSet2 = null;
         try {
-            connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM public.rates WHERE id = (?)");
 
             statement.setInt(1, 1);
@@ -72,6 +77,7 @@ public class SecondTest {
                 resultSet.close();
             if (resultSet2 != null)
                 resultSet2.close();
+
         }
     }
 
@@ -82,7 +88,7 @@ public class SecondTest {
         String staticButtonXpath = "//*[@id=\"mm-2\"]/ul/ul/li[9]/a";
         String currencyValue = "//*[@id=\"mm-52\"]/ul/li[1]/a";
         String everyDayCurrencyValue = "//*[@id=\"mm-53\"]/ul/li[1]/a";
-        String chooseDate = "//*[@id=\"form0\"]/aside[2]/div/div/div/div/div[3]/div[2]/div/div[2]/span[3]";
+        String chooseDate = "//*[@id=\"form0\"]/aside[2]/div/div/div/div/div[3]/div[2]/div/div[2]/span[1]";
         // TABLE
         String currencyTableXpath = "//*[@id=\"ratesData\"]/table/tbody/";
         String dollarXpath = currencyTableXpath + "tr[6]/td[2]";
@@ -118,26 +124,30 @@ public class SecondTest {
         WebElement euro = driver.findElement(By.xpath(euroXpath));
         WebElement valueE = driver.findElement(By.xpath(euroValueXpath));
         String usd = dollar.getText();
-        String usdPrice = valueD.getText();
+        String usdPriceActual = valueD.getText();
         String eur = euro.getText();
-        String eurPrice = valueE.getText();
-        System.out.println(usd + "=" + usdPrice + ";" + " " + eur + "=" + eurPrice);
+        String eurPriceActual = valueE.getText();
+        System.out.println(usd + "=" + usdPriceActual + ";" + " " + eur + "=" + eurPriceActual);
 
 
         File file;
-        file = new File(result);
+        file = new File(dataFromTheSite);
         FileWriter fw = new FileWriter(file);
 
         BufferedWriter writer = new BufferedWriter(fw);
-        writer.write(usd + "=" + usdPrice + ";" + " " + eur + "=" + eurPrice);
+        writer.write(usd + "=" + usdPriceActual + ";" + " " + eur + "=" + eurPriceActual);
         writer.close();
 
         driver.quit();
 
-        assertEquals(expectedUSD, usdPrice);
-        assertEquals(expectedEUR, eurPrice);
-        assertEquals(usdFromDatabase, usdPrice);
-        assertEquals(eurFromDatabase, eurPrice);
+        System.out.println(usd + "=" + usdFromFile + ";" + " " + eur + "=" + eurFromFile);
+        System.out.println(usd + "=" + usdFromDatabase + ";" + " " + eur + "=" + eurFromDatabase);
+
+        assertEquals(usdFromFile, usdPriceActual);
+        assertEquals(eurFromFile, eurPriceActual);
+        assertEquals(usdFromDatabase, usdPriceActual);
+        assertEquals(eurFromDatabase, eurPriceActual);
 
     }
 }
+
